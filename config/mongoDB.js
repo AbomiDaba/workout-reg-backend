7f2eff2d8dcdb870458319a38b20e1176a372f0a
Abomi Daba <abomi.daba@gmail.com>
2023-02-07 12:23:00 -0800

    fixed error

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const db = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://abomi_daba:t0M5fr8or9mLzxoz@mernapp.zpnw7ml.mongodb.net/?retryWrites=true&w=majority')
+        const conn = await mongoose.connect(process.env.ATLAS_URI)
         console.log('Successfully connected to MongoDB')
     } catch (error) {
         console.log(error)
